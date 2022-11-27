48411a2fcaaa6ba501376d0fe581684f4e4c35bf
FlakoBB <josephrbb99@gmail.com>
2022-11-27 12:35:35 -0600

    Ejercicio 1 - Modulos - Python

---
+++ Curso de Python/07-modulos/Ejercicio1/main.py
@@ -0,0 +1,13 @@
+import operBasicas as calcula
+
+sum = calcula.suma(6, 2)
+print("6 + 2 = ", sum)
+
+res = calcula.resta(6, 2)
+print("6 - 2 = ", res)
+
+mul = calcula.multiplica(6, 2)
+print("6 * 2 = ", mul)
+
+div = calcula.divide(6, 2)
+print("6 / 2 = ", div)
