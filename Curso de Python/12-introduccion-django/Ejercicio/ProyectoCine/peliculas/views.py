031020cec3dbb193f6b09ed5ac752a8d64c24c8f
FlakoBB <josephrbb99@gmail.com>
2022-12-06 14:35:54 -0600

    Ejercicio - Introduccion a Django - Python

--- Curso de Python/12-introduccion-django/Ejercicio/ProyectoCine/peliculas/views.py
+++ Curso de Python/12-introduccion-django/Ejercicio/ProyectoCine/peliculas/views.py
@@ -17,11 +17,14 @@ def index(request):
 def pelisDirector(request):
     id_director = request.POST['dir_id']
     titulos = Peliculas.objects.filter(director_id=id_director)
-
+    datos_dir = Directores.objects.filter(id=id_director)
+    total_pel = Peliculas.objects.filter(director_id=id_director).count()
     return render (
         request,
         'peliculs.html',
         context={
             'titulos': titulos,
+            'datos': datos_dir,
+            'total': total_pel,
         }
     )
