f52039f25d857ce955c9bd6754190dc2ad258fae
FlakoBB <josephrbb99@gmail.com>
2022-12-08 21:10:06 -0600

    Ejercicio - Sintaxis y Variables - JS

---
+++ Curso de JavaScript Basico/02-sintaxis-variables/Ejercicio/MisDatos.js
@@ -0,0 +1,18 @@
+
+const mis_datos = [
+    "Joseph Ryan",
+    23, 
+    true, 
+    Date("october 30 1999"), 
+    {
+        titulo: "El Viejo y el Mar", 
+        autor: "Ernest Hemingway", 
+        fecha: {
+            escrito: 1951, 
+            publicacion: 1952
+        }, 
+        url: "www.compra-el-viejo-y-el-mar.com"
+    }
+];
+
+console.log(mis_datos);
