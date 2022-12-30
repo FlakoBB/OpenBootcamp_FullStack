44edfc15642f76dcad5163a3c584a4169df5ae4a
FlakoBB <josephrbb99@gmail.com>
2022-12-29 18:17:37 -0600

    Ejercicio - Persistencia de Datos - JS

---
+++ Curso de JavaScript Basico/15-persistencia-datos/ejercicio/index.js
@@ -0,0 +1,10 @@
+const nom = "Flako"
+const ape = "BB"
+
+const persona = {nombre: nom, apellido: ape}
+
+// localStorage.setItem("personaLocal", JSON.stringify(persona))
+
+// sessionStorage.setItem("personaSession", JSON.stringify(persona))
+
+// document.cookie = `personaCookie=${JSON.stringify(persona)};expires=${new Date().setMinutes(new Date().getMinutes()+2)}`
