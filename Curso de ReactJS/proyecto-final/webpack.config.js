e506feac376ee828e6caaed0635a637bf99a04d9
FlakoBB <josephrbb99@gmail.com>
2023-05-15 18:21:32 -0600

    ReactJS Sesion29 Correccion Plantilla de proyecto

--- Curso de ReactJS/proyecto-final/webpack.config.js
+++ Curso de ReactJS/proyecto-final/webpack.config.js
@@ -1,94 +1,101 @@
-const path = require('path')
+const path = require('path');
 
-//? PLUGINS Y MINIFICADORES DE CSS Y SASS|SCSS
-// para reducir el tamaño de las hojas de estilo de nuestro proyecto
-const HtmlWebpackPlugin = require('html-webpack-plugin') //* para el template de html que usara webpack
-const MiniCssExtractPlugin = require('mini-css-extract-plugin') //* para reducir los css
-const { SourceMapDevToolPlugin } = require('webpack') //* para conopcer el source map de nuestro proyecto
+// PLUGINS Y MINIFICADORES DE CSSS Y SCSS/SASS
+// Para reducir el tamaño de las hojas de estilo de nuestro proyecto
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // Para el template del HTML que va a usar Webpack
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // Para reducir los CSS
+const { SourceMapDevToolPlugin } = require('webpack'); // Para conocer el Source Map de nuestro proyecto
 
-//? Configuracion del puerto
-const port = process.env.PORT || 3000 // encaso de existir variable de entorno 'PORT', usalo, sino usa el puerto 3000
+// Configuraciones de puerto
+const port = process.env.PORT || 3000;
 
-//? Exportar configuracion de Webpack
+// Exportar configuración de WebPack
 module.exports = {
+
     entry: './src/index.jsx',
     output: {
         path: path.join(__dirname, '/dist'),
         filename: 'bundle.[hash].js',
-        publicPath: '/'
+        publicPath: '/',
     },
     context: path.resolve(__dirname),
     devServer: {
         port,
-        inline: true,
-        historyApiFallback: true
+        historyApiFallback: true,
     },
     devtool: 'eval-source-map',
     module: {
         rules: [
-            //* Reglas para archivos JS y JSX
-            // tienen que pasar el LINTING para poder pasar
+            // Reglas para archivos de JS y JSX
+            // Tienen que pasar el LINTING para para poder pasar
             {
                 enforce: 'pre',
                 test: /(\.js|\.jsx)$/,
                 exclude: /node_modules/,
                 use: [
-                    'eslint-loader',
-                    'source-map-loader'
-                ]
+                  'eslint-loader',
+                  'source-map-loader',
+                ],
             },
-            // reglas de Babel ES y JSX
+            // Reglas para archivos JS y JSX
+            // Reglas de Babel ES y JSX
             {
-                test: /(\.js|\.jsx)$/,
+                test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                loader: 'babel-loader',
-                query: {
-                    presets: [
-                        '@babel/env',
-                        '@babel/react'
-                    ]
-                }
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/env',
+                            '@babel/react',
+                          ],
+                    },
+                },
             },
-            // reglas para CSS y SASS/SCSS
+            // Reglas para archivos CSS y SASS y SCSS para minificarlos y cargarlos en el bundle
             {
-                test: /(\.css|\.sass|\.scss)$/,
-                loader: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader'
-                ]
-            },
-            // reglas para los archivos de imagenes
-            {
-                test: /\.(png|jpe?g|gif)$/,
+                test: /(\.css|\.scss)$/,
                 use: [
-                    {
-                        loader: 'file-loader'
-                    }
-                ]
-            }
-        ]
+                    { loader: MiniCssExtractPlugin.loader },
+                    { loader: 'css-loader' },
+                    { loader: 'sass-loader' },
+                ],
+            },
+            // Reglas para los archivos de imágenes
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                  {
+                    loader: 'file-loader',
+                  },
+                ],
+            },
+        ],
     },
     plugins: [
-        // Template HTML
         new HtmlWebpackPlugin(
-            {
-                template: './index.html'
-            }
+          {
+            template: './public/index.html',
+          },
         ),
         new MiniCssExtractPlugin(
-            {
-                filename: './css/styles.css'
-            }
+          {
+            filename: './css/styles.css',
+          },
         ),
         new SourceMapDevToolPlugin(
-            {
-                filename: '[file].map'
-            }
-        )
-    ],
-    resolve: {
-        extensions: ['.js', '.jsx', '.css', '.sass', '.scss'],
-        modules: ['node_modules']
-    }
-}
+          {
+            filename: '[file].map',
+          },
+        ),
+      ],
+      resolve: {
+        extensions: ['.js', '.jsx', '.css', '.scss'],
+        modules: [
+          'node_modules',
+        ],
+        alias: {
+          'react-redux': path.join(__dirname, '/node_modules/react-redux/dist/react-redux.min'),
+        },
+      },
+};
